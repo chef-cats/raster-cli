4ec87e4c95e049d0bad3c9c40918b6773d5bfb88
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-06-06 22:50:09 +0300

    Boost.Test main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <cstdlib>
+#define BOOST_TEST_MODULE test
+#include <boost/test/included/unit_test.hpp>
 
-int main()
-{
-	return EXIT_SUCCESS;
+BOOST_AUTO_TEST_CASE( test ) {
+    BOOST_CHECK(4 == 4);
 }

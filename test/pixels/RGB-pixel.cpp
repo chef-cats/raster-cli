8f06b2c44cb4a99b2b0fc5581f6d87d9a9f56645
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-13 04:34:01 +0300

    Add validation function

--- test/pixels/RGB-pixel.cpp
+++ test/pixels/RGB-pixel.cpp
@@ -7,6 +7,8 @@
 #include <cstdlib>
 #include <ostream>
 
+using boost::test_tools::predicate_result;
+
 using std::array;
 using std::generate;
 using std::ostream;
@@ -57,8 +59,33 @@ bool operator==(const RGBPixel& pixel_a, const RGBPixel& pixel_b) noexcept
 ostream& operator<<(ostream& out, const RGBPixel& rgb_pixel)
 {
 	return out << "(" << rgb_pixel.get_red() << ", " << 
-		rgb_pixel.get_green() << ", " << 
+		rgb_pixel.get_green() << ", " <<
 		rgb_pixel.get_blue() << ")";
+}
+
+predicate_result compare_pixel_rgb_values(const RGBPixel& pixel, 
+	unsigned char red, unsigned char green, unsigned char blue) noexcept
+{
+	predicate_result are_equal = true;
+	if (pixel.get_red() != red) {
+		are_equal = false;
+		are_equal.message() << "\n" << 
+			"red = " << static_cast<unsigned int>(pixel.get_red()) << 
+			", expected " << static_cast<unsigned int>(red);
+	}
+	if (pixel.get_green() != green) {
+		are_equal = false;
+		are_equal.message() << "\n" << 
+			"green = " << static_cast<unsigned int>(pixel.get_green()) << 
+			", expected " << static_cast<unsigned int>(red);
+	}
+	if (pixel.get_blue() != blue) {
+		are_equal = false;
+		are_equal.message() << "\n" << 
+			"blue = " << static_cast<unsigned int>(pixel.get_blue()) << 
+			", expected " << static_cast<unsigned int>(red);
+	}
+	return are_equal;
 }
 
 BOOST_AUTO_TEST_SUITE(RGBPixelUnitTests)

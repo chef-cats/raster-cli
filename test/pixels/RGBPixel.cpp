6c4453fe03f48ca3a6aae5e7b7634bc3cea4363a
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-15 01:17:48 +0300

    Separate definition and declaration of helpers

--- test/pixels/RGBPixel.cpp
+++ test/pixels/RGBPixel.cpp
@@ -3,7 +3,6 @@
 
 #include <pixels/RGBPixel.hpp>
 
-#include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
 
 BOOST_AUTO_TEST_SUITE(RGBPixelUnitTests)
--- test/pixels/utils/Helpers.hpp
+++ test/pixels/utils/Helpers.hpp
@@ -1,50 +1,12 @@
 #pragma once
 
-#include <pixels/RGBPixel.hpp>
-
 #include <boost/test/unit_test.hpp>
 
-#include <ostream>
+class RGBPixel;
 
-using boost::test_tools::predicate_result;
+bool operator==(const RGBPixel& pixel_a, const RGBPixel& pixel_b) noexcept;
 
-using std::ostream;
+std::ostream& operator<<(std::ostream& out, const RGBPixel& rgb_pixel);
 
-static bool operator==(const RGBPixel& pixel_a, const RGBPixel& pixel_b) noexcept
-{
-	return pixel_a.get_red() == pixel_b.get_red() &&
-		pixel_a.get_green() == pixel_b.get_green() &&
-		pixel_a.get_blue() == pixel_b.get_blue();
-}
-
-static ostream& operator<<(ostream& out, const RGBPixel& rgb_pixel)
-{
-	return out << "(" << static_cast<unsigned int>(rgb_pixel.get_red()) << ", " <<
-		static_cast<unsigned int>(rgb_pixel.get_green()) << ", " <<
-		static_cast<unsigned int>(rgb_pixel.get_blue()) << ")";
-}
-
-static predicate_result compare_pixel_rgb_values(const RGBPixel& pixel,
-	unsigned char red, unsigned char green, unsigned char blue)
-{
-	predicate_result are_equal = true;
-	if (pixel.get_red() != red) {
-		are_equal = false;
-		are_equal.message() << "\n" <<
-			"red = " << static_cast<unsigned int>(pixel.get_red()) <<
-			", expected " << static_cast<unsigned int>(red);
-	}
-	if (pixel.get_green() != green) {
-		are_equal = false;
-		are_equal.message() << "\n" <<
-			"green = " << static_cast<unsigned int>(pixel.get_green()) <<
-			", expected " << static_cast<unsigned int>(red);
-	}
-	if (pixel.get_blue() != blue) {
-		are_equal = false;
-		are_equal.message() << "\n" <<
-			"blue = " << static_cast<unsigned int>(pixel.get_blue()) <<
-			", expected " << static_cast<unsigned int>(red);
-	}
-	return are_equal;
-}
+boost::test_tools::predicate_result compare_pixel_rgb_values(const RGBPixel& pixel,
+	unsigned char red, unsigned char green, unsigned char blue);

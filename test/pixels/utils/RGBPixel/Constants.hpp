294870c1fdfc12fea934a6d0289b1454f8f557c3
Ivan Mollov <ivan.xero.mollov@gmail.com>
2019-06-15 02:26:35 +0300

    Add new tests with fixed values // easy debugging

--- test/pixels/utils/RGBPixel/Constants.hpp
+++ test/pixels/utils/RGBPixel/Constants.hpp
@@ -2,4 +2,7 @@
 
 #include "BasicTestData.hpp"
 
-const BasicTestData<0> BASIC_TEST_DATA({}, {}, {});
+const BasicTestData<10> BASIC_TEST_DATA(
+	{129, 185, 111, 148, 248, 254, 154, 49,  4,   214},
+	{172, 194, 220, 37,  195, 60,  122, 242, 38,  254},
+	{175, 245, 35,  36,  59,  18,  144, 116, 187, 209});

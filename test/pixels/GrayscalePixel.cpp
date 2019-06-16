ab2e57f6fedf5f23f07c47770f071abc291f25a1
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-06-16 23:02:27 +0300

    Add a GrayscalePixel tests file.

---
+++ test/pixels/GrayscalePixel.cpp
@@ -0,0 +1,21 @@
+#include <pixels/GrayscalePixel.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+
+BOOST_AUTO_TEST_SUITE(GrayscalePixelUnitTests)
+
+BOOST_AUTO_TEST_SUITE(PositiveUnitTests)
+
+BOOST_AUTO_TEST_SUITE(BasicTests)
+
+BOOST_AUTO_TEST_CASE(EmptyTest)
+{
+   BOOST_CHECK(true == true);
+}
+
+BOOST_AUTO_TEST_SUITE_END(/*BasicTests*/)
+
+BOOST_AUTO_TEST_SUITE_END(/*PositiveUnitTests*/)
+
+BOOST_AUTO_TEST_SUITE_END(/*GrayscalePixelUnitTests*/)

2a97778bbde66256f314b82ff77f602bc97fb8c1
Yoanna Nikolova <nikolowa.yoanna@gmail.com>
2019-06-17 11:51:47 +0300

    Add MonochromePixel class

---
+++ src/include/pixels/MonochromePixel.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <pixels/Pixel.hpp>
+
+/**
+ * Class describing a Monochrome pixel.
+ *
+ * This class is inheritor of the Pixel and represent pixels
+ * which have 2 possible values - 0(white) and 1(black).
+ */
+
+class MonochromePixel {
+   enum class MonochromeValue : unsigned char;
+public:
+   MonochromePixel(unsigned char value);
+   MonochromePixel(const MonochromePixel& other) = default;
+   MonochromePixel(MonochromePixel&& other) = default;
+   MonochromePixel& operator=(const MonochromePixel& rhs) = default;
+   MonochromePixel& operator=(MonochromePixel&& rhs) = default;
+   virtual ~MonochromePixel() = default;
+
+public:
+   char get_value() const noexcept;
+   void set_value(unsigned char value);
+private:
+   MonochromeValue _value;
+};
+
+bool operator==(const MonochromePixel& lhs, const MonochromePixel& rhs);
+bool operator!=(const MonochromePixel& lhs, const MonochromePixel& rhs);
